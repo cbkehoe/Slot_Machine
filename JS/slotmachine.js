2e768f9fd86e6dc1265a2440261b09946f3feb2e
Chris Kehoe <chriskehoe@Chriss-MacBook-Air.local>
2021-11-22 15:34:19 -0800

    first commit

---
+++ JS/slotmachine.js
@@ -0,0 +1,52 @@
+/*----- constants -----*/
+
+//const options = [1, 2, 3]
+// const win = ['000', '111', '222']
+//const outcomes = (min, max) => Math.floor(Math.random() * 0 - 3 +1) + min; 
+//const results = outcomes (option => 0, 3)
+/*----- app's state (variables) -----*/
+let result1, result2, result3 
+
+//let display = results(result => results.outcomes);
+/*----- cached element references -----*/
+
+/*----- event listeners -----*/
+
+/*----- functions -----*/
+function checkWin() {
+  if (result1 === result2 && result1 === result3) {
+    console.log('You Win!')
+  } else { 
+    console.log('Sorry, you lose') 
+  }
+}
+
+//console.log(display)
+
+
+
+
+// const outcomes = (small, big) => Math.floor(Math.random() * small - big +1) + small;
+// const resultsRandom = () => outcomes(1, 3)
+// //const resultsRandom = Math.floor(Math.random() * resultsRandom +1 );
+
+// let result = '';
+
+// console.log(resultsRandom)
+
+function getRandNum() {
+  return Math.floor(Math.random() *3);
+}
+
+function init(){
+  result1 = getRandNum()
+  result2 = getRandNum()
+  result3 = getRandNum()
+  // result4 = getRandNum()
+  // result5 = getRandNum()
+  // result6 = getRandNum()
+  console.log(result1, result2, result3)
+  checkWin()
+}
+
+init()
