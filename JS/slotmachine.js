feac89f51a536eb99666bb524e9eb96ae375a41c
Chris Kehoe <chriskehoe@Chriss-MacBook-Air.local>
2021-11-23 17:02:25 -0800

    began work on a wallet that goes up or down by one based on the win loss occurence

--- JS/slotmachine.js
+++ JS/slotmachine.js
@@ -1,30 +1,29 @@
 /*----- constants -----*/
 
-//const options = [1, 2, 3]
-// const win = ['000', '111', '222']
-//const outcomes = (min, max) => Math.floor(Math.random() * 0 - 3 +1) + min; 
-//const results = outcomes (option => 0, 3)
-/*----- app's state (variables) -----*/
-let result1, result2, result3, outcomeMessage
+let result1, result2, result3, outcomeMessage, walletAmount
 
-//let display = results(result => results.outcomes);
+
 /*----- cached element references -----*/
 const slot1 = document.getElementById('slot1')
 const slot2 = document.getElementById('slot2')
 const slot3 = document.getElementById('slot3')
 const outcomes = document.getElementById('outcomes')
+
 /*----- event listeners -----*/
 document.querySelector('button').addEventListener('click', () => {
     init()
     render()
 })
 
+// $("button").click(function(){
+//     $("panel").animate({left: '250px'});
+//   }); 
 /*----- functions -----*/
 function checkWin() {
   if (result1 === result2 && result1 === result3) {
     outcomeMessage = 'You Win!'
   } else {
-    outcomeMessage = 'Sorry, You Lose'
+    outcomeMessage = 'Sorry Loser'
   }
   render()
 }
@@ -34,21 +33,17 @@ function render() {
     slot2.innerText = result2
     slot3.innerText = result3
     outcomes.innerText = outcomeMessage
+  
 }
 
-//document.write(result1, result2, result3);
-//console.log(display)
+// let walletAmount = 10;
 
-
-
-
-// const outcomes = (small, big) => Math.floor(Math.random() * small - big +1) + small;
-// const resultsRandom = () => outcomes(1, 3)
-// //const resultsRandom = Math.floor(Math.random() * resultsRandom +1 );
-
-// let result = '';
-
-// console.log(resultsRandom)
+// function earnLoseMoney() {
+//     if (result1 === result2 && result1 === result3) {
+//         walletAmount ++1;
+// } else {
+//     walletAmount (-1);
+// }
 
 function getRandNum() {
   return Math.floor(Math.random() *3);
@@ -58,11 +53,6 @@ function init(){
   result1 = getRandNum()
   result2 = getRandNum()
   result3 = getRandNum()
-  // result4 = getRandNum()
-  // result5 = getRandNum()
-  // result6 = getRandNum()
-  console.log(result1, result2, result3)
+
   checkWin()
 }
-
-init()
